e6df37adc35e09d6256caffaa08bdb894e6f70b6
SeanGau <rrtw0627@gmail.com>
2025-12-11 14:25:46 +0000

    Deploying to gh-pages from @ g0v/summit2026@ba2c3d5bc52e3ec7c943bfb19ecb9a2d40271145 🚀

--- assets/js/index.js
+++
@@ -1,7 +0,0 @@
-$(() => {
-  const currentTimestamp = Date.now()
-  const targetTimestamp = new Date('2026/01/05').getTime()
-  const diff = targetTimestamp - currentTimestamp
-  const deltaDays = Math.floor(diff / (1000 * 60 * 60 * 24))
-  $('#timer span').html(deltaDays)
-})
