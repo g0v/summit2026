24860ae9d207305db1d641c48ae196e6c3e08433
SeanGau <rrtw0627@gmail.com>
2026-03-06 21:29:13 +0800

    chore: add ticket bubble

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,9 +1,11 @@
 setInterval(() => {
-  $("#top-block .stool").each(function () {
+  $("#top-block .stool-container").each(function () {
     setTimeout(() => {
       $(this).addClass("animate");
+      $('.speech-bubble', this).addClass("animate");
       setTimeout(() => {
         $(this).removeClass("animate");
+        $('.speech-bubble', this).removeClass("animate");
       }, 1000)
     }, Math.random() * 2000);
   })
